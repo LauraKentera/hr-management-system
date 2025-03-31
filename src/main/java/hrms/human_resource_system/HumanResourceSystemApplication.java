c9f5559885af679c09a65763fc3983aa070638cd
Laura Kentera <lk7653@rit.edu>
2025-03-31 14:13:00 +0200

    Spring Initilzr Skeleton

---
+++ src/main/java/hrms/human_resource_system/HumanResourceSystemApplication.java
@@ -0,0 +1,13 @@
+package hrms.human_resource_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceSystemApplication.class, args);
+	}
+
+}
